b733bc809117c145f5c1bcfab535b456637c0f2b
xiwei <xiwei989@gmail.com>
2024-06-19 20:53:55 -0700

    test webhook

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -1,13 +1,24 @@
 package com.javatechie;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @RestController
 public class DevopsIntegrationApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(DevopsIntegrationApplication.class);
+
+	@PostConstruct
+	public void init() {
+		logger.info("Application started");
+	}
 
 	@GetMapping
 	public String message(){
